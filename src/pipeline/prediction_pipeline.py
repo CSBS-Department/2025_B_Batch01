9953b7985cac798127a99a38267f5ddf3a415c3b
Roffin7 <roffinjason@gmail.com>
2024-02-22 17:09:37 +0530

    partially prediction_pipeline completed

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -4,3 +4,24 @@ import pandas as pd
 from src.exception.exception import customexception
 from src.logger.logging import logging
 from src.utils.utils import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            preprocessor_path=os.path.join("artifacts","preprocessor.pkl")
+            model_path=os.path.join("artifacts","model.pkl")
+
+            preprocessor=load_object(preprocessor_path)
+            model=load_object(model_path)
+
+            scaled_fea=preprocessor.transform(features)
+            pred=model.predict(scaled_fea)
+
+            return pred
+
+        except Exception as e:
+            raise customexception(e,sys)
